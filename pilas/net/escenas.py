d8f78e467bb720999c6bc5ad35f63ab3dc3e0dc7
quiqueporta <devnull@localhost>
2012-04-13 21:40:29 +0200

    Arreglado el problema de crear los actores en los clientes nuevos.

--- pilas/net/escenas.py
+++ pilas/net/escenas.py
@@ -8,7 +8,6 @@
 
 import pilas
 from pilas.escenas import Normal
-from pilas.actores.actor import Actor
 
 from PodSixNet.Server import Server
 from PodSixNet.Channel import Channel
@@ -19,50 +18,43 @@ from weakref import WeakKeyDictionary
 import random
 
 from pilas.actores import *
-import uuid
+import uuid    
 
-class ClientChannel(Channel):
+class CanalCliente(Channel):
     """
-    This is the server representation of a single connected client.
+    Esta es la representación de canal de comunicación con el Cliente.
+    Aquí estan las posibles llamadas que hacen los Clientes al servidor.
     """
     def __init__(self, *args, **kwargs):
         Channel.__init__(self, *args, **kwargs)
     
     def Network_crear_actor(self, data):
+        """ Manda al resto de Clientes la creación de un Actor """
+        # Enviamos el comando al resto de clientes.
         self._server.enviar_al_resto(data, self)
-        self._server.actores.append({"clase" : data['clase'], "id" : data['id'], "cliente" : self.addr})
+        # El servidor se guarda la referencia del ID del Actor y que Cliente lo ha creado.
+        self._server.actores.append({"id" : data['id'], "cliente" : self.addr})
 
     def Network_mover_actor(self, data):
+        """ Manda la acción de mover un actor en el resto de Clientes """
         self._server.enviar_al_resto(data, self)
+
+    def Network_crear_actor_en_cliente(self, data):
+        """ Manda a un Cliente en concreto la creación de un Actor """
+        data['action'] = 'crear_actor'
+        # Enviamos el comando al resto de clientes.
+        self._server.enviar_a_cliente(data)
 
     def Close(self):
         pass
-    
 
-class EscucharServidor(ConnectionListener):
-    def __init__(self):
-        pass
-    
-    def Network_crear_actor(self, data):
-        clase_actor = eval(data['clase'])
-        actor = clase_actor()
-        actor.id = data['id']
-        actor.x = data['x']
-        actor.y = data['y']
-        self._actores_ajenos.append(actor)
-    
-    def Network_mover_actor(self, data):
-        for actor in self._actores_ajenos:
-            if (isinstance(actor, Actor) and actor.id == data['id']):
-                actor.x = data['x'] 
-                actor.y = data['y']
-                actor.escala_x = data['escala_x']
-                actor.escala_y = data['escala_y']
-                break
+# ---------------------------------
+# SERVIDOR
+# ---------------------------------
 
 class ServidorPilas(Server):
 
-    channelClass = ClientChannel
+    channelClass = CanalCliente
     
     def __init__(self, puerto_servidor=31425):
         Server.__init__(self, localaddr=(pilas.net.obteber_ip_local(), puerto_servidor))
@@ -80,35 +72,84 @@ class ServidorPilas(Server):
     def Connected(self, channel, addr):
         print "Cliente agregado"
         self.agregar_Cliente(channel)
-        for actor in self.actores:
-            channel.Send({"action": "crear_actor", "clase" : actor['clase'], "id" : actor['id']})
-    
+            
     def agregar_Cliente(self, cliente):
         print "Nuevo Cliente " + str(cliente.addr)
         self._clientes[cliente] = True
-        #self.SendPlayers()
-        #print "clientes", [p for p in self._clientes]        
+        self.enviar_actores_a_cliente(cliente)
     
     def eliminar_Cliente(self, cliente):
         print "Eliminando Cliente " + str(cliente.addr)
         del self._clientes[cliente]
-        #self.SendPlayers()
-    
-    def SendPlayers(self):
-        self.SendToAll({"action": "players", "players": [p.nickname for p in self.players]})
+
+    def enviar_actores_a_cliente(self, cliente):
+        for c in self._clientes:
+            if (c != cliente):
+                c.Send({"action" : "enviar_actores_a_cliente",
+                        "cliente" : cliente.addr})
+        
     
     def enviar_al_resto(self, data, cliente_excepcion):
         for c in self._clientes:
             if (c != cliente_excepcion):
                 c.Send(data)
         
-    
     def enviar_a_todos(self, data):
         [c.Send(data) for c in self._clientes]
-    
+        
+    def enviar_a_cliente(self, data):
+        for c in self._clientes:
+            if (c.addr == data['cliente']):
+                print data
+                c.Send(data)
+        
+
+# ---------------------------------
+# CLIENTE
+# ---------------------------------
+
 class ActorObserver():
     def cambioEnActor(self, event):
         print "Debe implementar el metodo cambioEnActor para Observar los cambios de los actores."    
+
+class EscucharServidor(ConnectionListener):
+    """ 
+    Clase que implementa las peticiones que le llegan desde el servidor.    
+    """
+        
+    def Network_crear_actor(self, data):
+        """ Crea un actor de otro Cliente """
+        clase_actor = eval(data['clase'])
+        actor = clase_actor()
+        actor.id = data['id']
+        actor.x = data['x']
+        actor.y = data['y']
+        actor.escala_x = data['escala_x']
+        actor.escala_y = data['escala_y']
+        self._actores_ajenos.append(actor)
+    
+    def Network_mover_actor(self, data):
+        """ Mueve un actor de otro cliente """
+        for actor in self._actores_ajenos:
+            if (isinstance(actor, Actor) and actor.id == data['id']):
+                actor.x = data['x'] 
+                actor.y = data['y']
+                actor.escala_x = data['escala_x']
+                actor.escala_y = data['escala_y']
+                break
+            
+    def Network_enviar_actores_a_cliente(self, data):
+        """ Envia todos sus actores a otro Cliente """
+        for actor in self._actores_compartidos:
+            if (isinstance(actor, Actor)):
+                connection.Send({"action" : "crear_actor_en_cliente",
+                                 "cliente" : data['cliente'],
+                                 "clase" : actor.__class__.__name__ , 
+                                 "id" : actor.id,
+                                 "x" : actor.x,
+                                 "y" : actor.y,
+                                 "escala_x" : actor.escala_x,
+                                 "escala_y" : actor.escala_y})
 
 class EscenaNetwork(Normal, EscucharServidor, ActorObserver):
     
@@ -139,11 +180,14 @@ class EscenaNetwork(Normal, EscucharServidor, ActorObserver):
             for actor in self._actores_compartidos:
                 if (isinstance(actor, Actor) and actor.id == ""):
                     actor.id = str(uuid.uuid4())
-                    connection.Send({"action": "crear_actor", 
-                                     "clase" : actor.__class__.__name__ , 
-                                     "id" : actor.id,
-                                     "x" : actor.x,
-                                     "y" : actor.y})
+                    print "crear_actor"
+                    connection.Send({"action" : "crear_actor",
+                                 "clase" : actor.__class__.__name__ , 
+                                 "id" : actor.id,
+                                 "x" : actor.x,
+                                 "y" : actor.y,
+                                 "escala_x" : actor.escala_x,
+                                 "escala_y" : actor.escala_y})
         connection.Pump()
         self.Pump()
         
